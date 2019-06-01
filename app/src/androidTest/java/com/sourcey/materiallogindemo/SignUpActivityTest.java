96ba36411f34a2467485d6048d72e7db133b01be
smilesmad <smilesmad@gmail.com>
2019-06-02 01:46:19 +0700

    Check Error

--- app/src/androidTest/java/com/sourcey/materiallogindemo/SignUpActivityTest.java
+++ app/src/androidTest/java/com/sourcey/materiallogindemo/SignUpActivityTest.java
@@ -20,6 +20,7 @@ import android.widget.Toast;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -31,6 +32,7 @@ import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -42,45 +44,82 @@ import static org.hamcrest.Matchers.is;
 @RunWith(AndroidJUnit4.class)
 public class SignUpActivityTest {
 
+    public static String name;
+    public static String address;
+    public static String email;
+    public static String mobile;
+    public static String password;
+
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    public String name = "aaa";
-    public String address = "aaa";
-    public String email = "test@test.com";
-    public String mobile = "0123456789";
-    public String password = "0123456789";
+    @Before
+    public void Setup() {
+        name = "aaa";
+        address = "aaa";
+        email = "test@test.com";
+        mobile = "0123456789";
+        password = "0123456789";
+    }
 
     @Test
-    public void signUpActivityTest() {
-        onView(withId(R.id.link_signup)).perform(click());
-        onView(withId(R.id.input_name)).perform(scrollTo(), typeText(name), closeSoftKeyboard());
-        onView(withId(R.id.input_address)).perform(scrollTo(), typeText(address), closeSoftKeyboard());
-        onView(withId(R.id.input_email)).perform(scrollTo(), typeText(email), closeSoftKeyboard());
-        onView(withId(R.id.input_mobile)).perform(scrollTo(), typeText(mobile), closeSoftKeyboard());
-        onView(withId(R.id.input_password)).perform(scrollTo(), typeText(password), closeSoftKeyboard());
-        onView(withId(R.id.input_reEnterPassword)).perform(scrollTo(), typeText(password), closeSoftKeyboard());
-        onView(withId(R.id.btn_signup)).perform(scrollTo(), click());
+    public void signUp_Success() {
+        signUpAction();
 
         waitPageLoading();
         validateMainActivity();
 
-        //onView(withId(R.id.btn_logout)).perform(click());
+        onView(withId(R.id.btn_logout)).perform(click());
+    }
+
+    @Test
+    public void signUp_NameFailed() {
+        name = "";
+        signUpAction();
+        onView(withId(R.id.input_name)).check(matches(hasErrorText("at least 3 characters")));
     }
 
     @Test
     public void login_Success(){
         loginAction(email, password);
         waitPageLoading();
-
         validateMainActivity();
-        //onView(withId(R.id.btn_logout)).perform(click());
     }
 
     @Test
-    public void login_Failure(){
+    public void login_InvalidEmail(){
         loginAction("test", "test1234");
-        //onView(withText(R.string.)).inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView()))).check(matches(isDisplayed()))
+        onView(withId(R.id.input_email)).check(matches(hasErrorText("enter a valid email address")));
+    }
+
+    @Test
+    public void login_InvalidPasswordMinLength(){
+        loginAction(email, "tes");
+        onView(withId(R.id.input_password)).check(matches(hasErrorText("between 4 and 10 alphanumeric characters")));
+    }
+
+    @Test
+    public void login_InvalidPasswordMaxLength(){
+        loginAction(email, "01234567891");
+        onView(withId(R.id.input_password)).check(matches(hasErrorText("between 4 and 10 alphanumeric characters")));
+    }
+
+    @Test
+    public void login_InvalidPassword(){
+        loginAction(email, "test");
+        onView(withId(R.id.input_password)).check(matches(hasErrorText("enter a valid email address or password")));
+    }
+
+    @Test
+    public void login_EmptyEmail(){
+        loginAction("", password);
+        onView(withId(R.id.input_email)).check(matches(hasErrorText("enter a valid email address")));
+    }
+
+    @Test
+    public void login_EmptyPassword(){
+        loginAction(email, "");
+        onView(withId(R.id.input_password)).check(matches(hasErrorText("between 4 and 10 alphanumeric characters")));
     }
 
     private static void loginAction(String email, String password){
@@ -104,23 +143,15 @@ public class SignUpActivityTest {
             e.printStackTrace();
         }
     }
-/*
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
 
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }*/
+    private void signUpAction() {
+        onView(withId(R.id.link_signup)).perform(click());
+        onView(withId(R.id.input_name)).perform(scrollTo(), typeText(name), closeSoftKeyboard());
+        onView(withId(R.id.input_address)).perform(scrollTo(), typeText(address), closeSoftKeyboard());
+        onView(withId(R.id.input_email)).perform(scrollTo(), typeText(email), closeSoftKeyboard());
+        onView(withId(R.id.input_mobile)).perform(scrollTo(), typeText(mobile), closeSoftKeyboard());
+        onView(withId(R.id.input_password)).perform(scrollTo(), typeText(password), closeSoftKeyboard());
+        onView(withId(R.id.input_reEnterPassword)).perform(scrollTo(), typeText(password), closeSoftKeyboard());
+        onView(withId(R.id.btn_signup)).perform(scrollTo(), click());
+    }
 }
