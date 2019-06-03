2bd9c329229ee9f492b5ed0b32136218b0ddb0c3
smilesmad <smilesmad@gmail.com>
2019-06-03 23:39:23 +0700

    comment unused package/class

--- app/src/androidTest/java/com/sourcey/materiallogindemo/ApplicationTest.java
+++ app/src/androidTest/java/com/sourcey/materiallogindemo/ApplicationTest.java
@@ -13,7 +13,7 @@ import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
+//import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 //import org.junit.runner.Description;
